55068f2b621a306eff78337c325fbc6846211375
Edward Qian <edward.c.qian@gmail.com>
2019-11-20 20:21:50 -0500

    updated setup

--- setup.sh
+++ setup.sh
@@ -7,10 +7,10 @@ cd gpt2
 
 git checkout d98291d2ae0761eff6d12f0e4e52e93c7e847eb2
 
-# python download_model.py 124M
-# python download_model.py 355M
-# python download_model.py 774M
-# python download_model.py 1558M
+python download_model.py 124M
+python download_model.py 355M
+python download_model.py 774M
+python download_model.py 1558M
 
 cd ..
 
