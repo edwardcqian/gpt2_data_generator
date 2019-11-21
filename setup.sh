8fe7aeaf9666cceb7d680cb3fcaa572ee7649a03
Edward Qian <edward.c.qian@gmail.com>
2019-11-20 20:23:52 -0500

    Base generator (#2)
    * added basic generator and setup files
    
    * updated generator
    
    * added data folder updated readme
    
    * README update
    
    * remove uncessarily folder
    
    * moved script into source folder
    
    * added env variable in run script
    
    * updated readme
    
    * large model config
    
    * updated readme with config
    
    * updated setup

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+mkdir gpt2
+
+git clone https://github.com/openai/gpt-2.git gpt2
+
+cd gpt2
+
+git checkout d98291d2ae0761eff6d12f0e4e52e93c7e847eb2
+
+python download_model.py 124M
+python download_model.py 355M
+python download_model.py 774M
+python download_model.py 1558M
+
+cd ..
+
+DOCKERFILE=${1:-Dockerfile.cpu}
+
+IMAGENAME=${2:-gpt2_cpu}
+
+docker build -f $DOCKERFILE -t $IMAGENAME gpt2
